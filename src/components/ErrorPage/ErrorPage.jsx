b94ad442593fc0f678c93181294e75d3873a0dfc
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-06-01 18:12:46 +0200

    add error page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,33 @@
-const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+import {
+  HomeTitle,
+  HomeWrapper,
+  HomeTextWrapper,
+  HomeAuthLinks,
+  HomeAuthLinksList,
+  HomeDescription,
+  UserLogo,
+} from 'pages/Home/Home.styled';
+import { MdReportGmailerrorred } from 'react-icons/md';
+
+export const ErrorPage = () => {
+  return (
+    <HomeWrapper>
+      <UserLogo>
+        <MdReportGmailerrorred size="200px" />
+      </UserLogo>
+      <HomeTextWrapper>
+        <HomeTitle>Error 404</HomeTitle>
+        <HomeDescription>
+          Sorry, page is not found. Try navigate to home page.
+        </HomeDescription>
+        <HomeAuthLinksList>
+          <li>
+            <HomeAuthLinks to="/">Home</HomeAuthLinks>
+          </li>
+        </HomeAuthLinksList>
+      </HomeTextWrapper>
+    </HomeWrapper>
+  );
 };
 
 export default ErrorPage;
